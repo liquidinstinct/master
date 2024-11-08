19e5e9b714712d17f02d1011a88b1130389b0f8e
dan <94872278+liquidinstinct@users.noreply.github.com>
2024-11-09 05:37:21 +1300

    Delete ecu_tuner_project_refined_package directory

--- ecu_tuner_project_refined_package/ecu_handlers/tactrix_handler.py
+++
@@ -1,8 +0,0 @@
-from tactrix_handler import TactrixHandler
-
-tactrix = TactrixHandler()
-rpm_data = tactrix.read_data('010C')  # Example command to read RPM
-if rpm_data:
-    print(f"RPM data: {rpm_data}")
-tactrix.disconnect()
-
